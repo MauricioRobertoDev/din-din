9cc471105a85271bdfa5a6dcdb94f09f5802c35c
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-02-24 14:54:38 -0300

    DIN-9 configuração do breeze

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail as AuthMustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements AuthMustVerifyEmail
 {
     use HasApiTokens;
     use HasFactory;
