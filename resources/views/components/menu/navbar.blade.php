ba358db7cd3630a1fc6aea0ada35607092308550
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-03-08 14:16:27 -0300

    DIN-18 ao abrir o menu no mobile ele fica por cima da topbar

--- resources/views/components/menu/navbar.blade.php
+++ resources/views/components/menu/navbar.blade.php
@@ -1,3 +1,27 @@
+{{-- top bar --}}
+<div class="fixed top-0 left-0 z-50 flex items-center justify-between w-full h-16 px-4 bg-white shadow grow-0 lg:hidden">
+
+    <button type="button">
+        <x-icon.logo class="w-6 h-6 text-primary-500" />
+    </button>
+
+    <button
+        type="button"
+        x-show="!openMenu"
+        x-on:click.prevent="$dispatch('toggle-menu')"
+    >
+        <x-icon.bars class="w-6 h-6 text-gray-700" />
+    </button>
+
+    <button
+        type="button"
+        x-show="openMenu"
+        x-on:click.prevent="$dispatch('toggle-menu')"
+    >
+        <x-icon.x class="w-6 h-6 text-gray-700" />
+    </button>
+</div>
+
 {{-- navigation --}}
 <nav
     x-show="openMenu"
@@ -6,10 +30,10 @@
     x-transition:enter-end="opacity-100 left-0"
     x-transition:leave-start="opacity-100 left-0"
     x-transition:leave-end="opacity-0 -left-full"
-    class="fixed bg-white shadow-xl flex lg:!flex flex-col justify-between lg:relative top-0 z-50 flex-grow-0 h-screen pt-16 transition-all duration-200 transform lg:!left-0 lg:pt-0 w-80"
+    class="fixed bg-white shadow-xl flex lg:!flex flex-col justify-between lg:relative top-0 z-50 flex-grow-0 h-screen transition-all duration-200 transform lg:!left-0 pt-0 w-80"
 >
     <div class="flex-grow overflow-y-auto ">
-        <div class="hidden p-4 lg:block">
+        <div class="p-4">
             <x-icon.logo class="w-6 h-6 text-primary-500" />
         </div>
 
@@ -81,28 +105,3 @@
 
     </div>
 </nav>
-
-{{-- top bar --}}
-<div
-    class="fixed top-0 left-0 z-50 flex items-center justify-between w-full h-16 px-4 bg-white shadow grow-0 lg:hidden">
-
-    <button type="button">
-        <x-icon.logo class="w-6 h-6 text-primary-500" />
-    </button>
-
-    <button
-        type="button"
-        x-show="!openMenu"
-        x-on:click.prevent="$dispatch('toggle-menu')"
-    >
-        <x-icon.bars class="w-6 h-6 text-gray-700" />
-    </button>
-
-    <button
-        type="button"
-        x-show="openMenu"
-        x-on:click.prevent="$dispatch('toggle-menu')"
-    >
-        <x-icon.x class="w-6 h-6 text-gray-700" />
-    </button>
-</div>
