666048e770936c4207219055a2143d99bc7c1d4d
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-03-08 15:21:41 -0300

    DIN-12 testes para tela de login

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -5,31 +5,69 @@ declare(strict_types=1);
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 
-test('login screen can be rendered', function () {
-    $response = $this->get('/login');
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\assertAuthenticated;
+use function Pest\Laravel\assertGuest;
+use function Pest\Laravel\get;
+use function Pest\Laravel\post;
 
-    $response->assertStatus(200);
+test('A rota deve estar disponível e funcional', function () {
+    get(route('login'))
+        ->assertOk()
+        ->assertViewIs('auth.login');
 });
 
-test('users can authenticate using the login screen', function () {
+test('Usuário deve poder se autenticar', function () {
     $user = User::factory()->create();
-
-    $response = $this->post('/login', [
+    $data = [
         'email' => $user->email,
         'password' => 'password',
-    ]);
+    ];
 
-    $this->assertAuthenticated();
-    $response->assertRedirect(RouteServiceProvider::HOME);
+    assertGuest();
+
+    post(route('login'), $data)
+        ->assertRedirect(RouteServiceProvider::HOME);
+
+    assertAuthenticated();
 });
 
-test('users can not authenticate with invalid password', function () {
+test('Usuário não deve poder se autenticar com senha errada', function () {
     $user = User::factory()->create();
-
-    $this->post('/login', [
+    $data = [
         'email' => $user->email,
         'password' => 'wrong-password',
-    ]);
+    ];
 
-    $this->assertGuest();
+    assertGuest();
+
+    post(route('login'), $data)
+        ->assertSessionHasErrors();
+
+    assertGuest();
+});
+
+test('Usuário deve ser redirecionado caso já esteja logado', function () {
+    $user = User::factory()->create();
+    $data = [
+        'email' => $user->email,
+        'password' => 'wrong-password',
+    ];
+
+    actingAs($user);
+
+    assertAuthenticated();
+
+    post(route('login'), $data)
+        ->assertRedirect(route('dashboard'));
+});
+
+test('Usuário logado deve deslogar do sistema', function () {
+    $user = User::factory()->create();
+    actingAs($user);
+
+    assertAuthenticated();
+
+    post(route('logout'))
+        ->assertRedirect(route('root'));
 });
