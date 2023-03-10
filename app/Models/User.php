4bc185b4feb9a8aee3d37a9682415870ada1d647
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-03-10 15:29:39 -0300

    DIN-14 adição de relacionamentos com transactions

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,7 @@ use Illuminate\Auth\MustVerifyEmail;
 use Illuminate\Contracts\Auth\MustVerifyEmail as AuthMustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -64,5 +65,20 @@ class User extends Authenticatable implements AuthMustVerifyEmail
     public function accounts(): HasMany
     {
         return $this->hasMany(Account::class);
+    }
+
+    public function transactions(): HasManyThrough
+    {
+        return $this->hasManyThrough(Transaction::class, Account::class);
+    }
+
+    public function incomes(): HasManyThrough
+    {
+        return $this->hasManyThrough(Transaction::class, Account::class)->where('type', Transaction::TYPE_INCOME);
+    }
+
+    public function expenses(): HasManyThrough
+    {
+        return $this->hasManyThrough(Transaction::class, Account::class)->where('type', Transaction::TYPE_EXPENSE);
     }
 }
