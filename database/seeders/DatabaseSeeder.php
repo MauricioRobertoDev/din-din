dc2498f963422c251d52a829ee57a6637e67bfa0
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-03-10 15:03:34 -0300

    DIN-19 adição de category seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(PaymentTypeSeeder::class);
+        $this->call(CategorySeeder::class);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
