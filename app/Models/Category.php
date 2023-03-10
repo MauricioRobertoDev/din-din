a02d2351d7810ef41f3579b3d1545c4759984266
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-03-10 14:37:28 -0300

    DIN-16 adição de relacionamento com categorias

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -23,4 +23,9 @@ class Category extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(self::class);
+    }
 }
