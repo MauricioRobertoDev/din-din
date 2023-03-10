c900321c3442538d09cde75a5b90211e69d3590c
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-03-10 14:07:47 -0300

    DIN-16 criação da factory

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'category_id' => null,
+            'name' => fake()->word(),
+            'user_id' => User::factory()->create(),
+            'type' => fake()->randomElement(['income', 'expense']),
+        ];
+    }
+}
