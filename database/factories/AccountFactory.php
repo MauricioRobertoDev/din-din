aea901f333ec1e7b44c0c09a92599c34478d4c2e
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-03-10 15:01:19 -0300

    DIN-19 criação da factory

---
+++ database/factories/AccountFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Account>
+ */
+class AccountFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->word(),
+            'start_amount' => 0.00,
+            'balance' => 0.00,
+            'user_id'  => User::factory()->create(),
+        ];
+    }
+}
